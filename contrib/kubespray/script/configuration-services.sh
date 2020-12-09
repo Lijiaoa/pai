fee2d00f0ef4e1bbbbd4af2256af90e40f832afb
suiguoxin <suiguoxin@gmail.com>
2020-12-09 14:19:43 +0800

    fix

--- contrib/kubespray/script/configuration-services.sh
+++ contrib/kubespray/script/configuration-services.sh
@@ -17,6 +17,6 @@ done
 
 mkdir -p ${HOME}/pai-deploy/quick-start-config/
 cp ${LAYOUT} ${HOME}/pai-deploy/quick-start-config/layout.yaml
-cp ${CLUSTER_CONFIG} ${HOME}/pai-deploy/quick-start-config/config.yml
+cp ${CLUSTER_CONFIG} ${HOME}/pai-deploy/quick-start-config/config.yaml
 
 cp ${HOME}/pai-deploy/pai/contrib/kubespray/quick-start/services-configuration.yaml.template ${HOME}/pai-deploy/quick-start-config/
